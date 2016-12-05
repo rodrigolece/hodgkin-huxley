ddcaa0000bd38fb93658ceb97a6921a9fddc0a30
Rodrigo Leal Cervantes <rodrigolealc@ciencias.unam.mx>
2016-12-05 18:12:45 +0000

    hodgkinHuxley with varargout

--- hodgkinHuxley.m
+++ hodgkinHuxley.m
@@ -1,10 +1,15 @@
-function hodgkinHuxley()
+function varargout = hodgkinHuxley()
 
 global dvec end_time initial_vec error_tolerance
 
 [ts, vec] = ode45(dvec, [0 end_time], initial_vec, error_tolerance);
 Vs = vec(:,1);
 
-plot(ts, -Vs)
+if nargout == 0
+    plot(ts, -Vs)
+else
+    varargout{1} = ts;
+    varargout{2} = Vs;
+end
 
 end
