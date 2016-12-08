018649161177f09b27da128cf5316f01bda6ac1c
Rodrigo Leal Cervantes <rodrigolealc@ciencias.unam.mx>
2016-12-08 15:49:54 +0000

    Function to find threshold fow one stimulus

---
+++ findThreshold.m
@@ -0,0 +1,20 @@
+function [Amin, Amax] = findThreshold(deltaT, Amin, Amax, tol)
+
+span = Amax - Amin;
+
+while  span > tol
+    A = 0.5*(Amin + Amax);
+    
+    setupCurrent(A, deltaT)
+    [~, Vs] = hodgkinHuxley();
+    
+    if countActions(Vs) == 1
+        Amax = A;
+    else
+        Amin = A;
+    end
+    
+    span = Amax - Amin;
+end
+
+end
