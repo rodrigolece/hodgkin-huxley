de1a7e780967afa22852f2e1e466b97d3e906a5a
Rodrigo Leal Cervantes <rodrigolealc@ciencias.unam.mx>
2016-12-14 19:28:21 +0000

    Five argument use of hodgkinHuxley

--- hodgkinHuxley.m
+++ hodgkinHuxley.m
@@ -7,9 +7,15 @@ Vs = vec(:,1);
 
 if nargout == 0
     plot(ts, -Vs)
-else
+elseif nargout == 2
     varargout{1} = ts;
     varargout{2} = Vs;
+elseif nargout == 5
+    varargout{1} = ts;
+    varargout{2} = Vs;
+    varargout{3} = vec(:,2);
+    varargout{4} = vec(:,3);
+    varargout{5} = vec(:,4);
 end
 
 end
